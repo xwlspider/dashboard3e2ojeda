4c4779bce9eb81549c18b5cb85fb1a15bdcff50c
emyvale0811 <emyvale0811@gmail.com>
2025-09-17 16:02:13 +0000

    ✨ feat(auth): Se genero un nuevo README -  Se configuro desde 0 usando gemeni con ctrl + i

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center bg-yellow-100">
+      <Text>Hola Mundo</Text>
     </View>
   );
 }
