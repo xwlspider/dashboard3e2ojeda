d1ec94697008e029f372fb4ce667195b06f1c49c
emyvale0811 <emyvale0811@gmail.com>
2025-09-17 17:38:40 +0000

    💄iu(ProfileDetails): Se creo los detalles principales - Se añadio una imagen, nombre del usuario y rol

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,6 @@ import "../global.css";
 export default function Index() {
   return (
     <View className="flex justify-center items-center bg-stone-800 w-screen h-screen">
-      <Text className="text-white bold text-xl">Edit app/index.tsx to edit this screen.</Text>
     <View className="flex justify-center items-center bg-white w-screen h-screen">
       <ProfileDetails></ProfileDetails>
     </View>
--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Image, Text, View } from 'react-native'
+import "../global.css"
 
 const ProfileDetails = () => {
   return (
     <View>
-      <Text>ProfileDetails</Text>
-      <Image source={require('../assets/images/icon.png')}>
+     
+      <Image 
+      className='max-h-20 max-w-20 rounded-full'
+      source={require('../assets/images/icon.png')}
+      />
+       <Text className='text 2xl font-bold'>Erick Ojeda</Text> 
+       <Text className='text-xL font-bold text-gray-400'> Estudiante </Text>
     </View>
   )
 }
